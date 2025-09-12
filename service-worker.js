9c9638e7fb3934a63e17290c10097eff81b1b585
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-09-12 12:28:31 +0200

    Fixes #22

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'todoapp-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/styles.css',
+  '/assets/js/index.js',
+  // Add other JS files and assets you want to cache
+];
+
+/* self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+}); */
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
