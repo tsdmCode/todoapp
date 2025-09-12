703326502156e39ac535a67599957f6480736321
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-09-12 13:11:27 +0200

    CSS fixes

--- assets/js/populateList.js
+++ assets/js/populateList.js
@@ -18,7 +18,7 @@ export default function populateList() {
 
       list.innerHTML = element.name;
       list.dataset.id = element.id;
-
+      list.tabIndex = 1;
       list.addEventListener('click', (e) => {
         listHead.innerHTML = list.innerText;
 
@@ -28,22 +28,4 @@ export default function populateList() {
       listMenu.append(list);
     });
   }
-  // der her skal genbruges
-  /* lists.forEach((element) => {
-    //skaber listen's header
-    const list = document.createElement('ul');
-    const listTitle = document.createElement('li');
-    listTitle.classList.add('list-title');
-    listTitle.innerText = element.name;
-
-    //loop der smider hver task ind under den relevante liste
-    element.tasks.forEach((task) => {
-      const taskItem = document.createElement('li');
-      taskItem.innerText = task.text;
-      taskItem.classList.add(task.done ? 'finished' : 'unfinished');
-      listTitle.append(taskItem);
-    });
-    list.append(listTitle);
-    viewport.append(list);
-  }); */
 }
